81176f939e62c57665b4a6d52c828e3a1071b40e
alessandrak <alessandra.carneiro@ckl.io>
2023-01-23 19:06:06 -0300

    tests: add get main wallet balance tests

---
+++ backend/src/api/exchange/api/demo/tests/tests_get_main_wallet_eur_balance.py
@@ -0,0 +1,87 @@
+from django.test import override_settings
+from django.test.client import Client
+from django.urls import reverse
+from pytest_mock import MockerFixture
+from rest_framework import status
+from rest_framework.response import Response
+
+from api.exchange.use_cases.demo.tests.mocks import constants
+from api.exchange.use_cases.tests.mocks.utils import get_mocked_account_object
+from api.stellar.helpers.utils import get_network_data
+
+
+def get_main_wallet_eur_balance_request(
+    client: Client,
+) -> Response:
+    """
+    Make a request to get main wallet EUR balance
+    Args:
+        client: HTTP Client
+    """
+    return client.get(
+        path=reverse(
+            "exchange:get-main-wallet-eur-balance",
+        ),
+        content_type="application/json",
+    )
+
+
+@override_settings(
+    MAIN_WALLET_PK="GDIRWLYTROHTR42SZ2KYU4D6S2DGZIPALUWA4RBQIJLQZT3TLV7ADUTJ",
+    EUR_CODE="EUR",
+    EUR_ISSUER="GC3XSOYSQDYBBGBBMV7OBZJOZQL47POSV6R4WPPXQVYMIEQBDFTAK46U",
+)
+def test_get_main_wallet_eur_balance_successfully(
+    mocker: MockerFixture,
+    client: Client,
+) -> None:
+
+    acc_get_network_data_mock = mocker.patch(
+        "api.stellar.helpers.accounts.get_network_data",
+        side_effect=get_network_data,
+    )
+
+    load_account_mock = mocker.patch(
+        "stellar_sdk.Server.load_account",
+        return_value=get_mocked_account_object(data=constants.STELLAR_ACCOUNT_RESPONSE),
+    )
+
+    response = get_main_wallet_eur_balance_request(client=client)
+
+    load_account_mock.assert_called_once()
+    acc_get_network_data_mock.assert_called_once()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        "balance": "10000000000.0000000",
+        "asset_code": "EUR",
+        "asset_issuer": "GC3XSOYSQDYBBGBBMV7OBZJOZQL47POSV6R4WPPXQVYMIEQBDFTAK46U",
+    }
+
+
+@override_settings(
+    MAIN_WALLET_PK="GDIRWLYTROHTR42SZ2KYU4D6S2DGZIPALUWA4RBQIJLQZT3TLV7ADUTJ",
+    EUR_CODE="USD",
+    EUR_ISSUER="GC3XSOYSQDYBBGBBMV7OBZJOZQL47POSV6R4WPPXQVYMIEQBDFTAK46U",
+)
+def test_get_main_wallet_eur_balance_not_found(
+    mocker: MockerFixture,
+    client: Client,
+) -> None:
+
+    acc_get_network_data_mock = mocker.patch(
+        "api.stellar.helpers.accounts.get_network_data",
+        side_effect=get_network_data,
+    )
+
+    load_account_mock = mocker.patch(
+        "stellar_sdk.Server.load_account",
+        return_value=get_mocked_account_object(data=constants.STELLAR_ACCOUNT_RESPONSE),
+    )
+
+    response = get_main_wallet_eur_balance_request(client=client)
+
+    load_account_mock.assert_called_once()
+    acc_get_network_data_mock.assert_called_once()
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
--- backend/src/api/exchange/use_cases/demo/tests/mocks/constants.py
+++ backend/src/api/exchange/use_cases/demo/tests/mocks/constants.py
@@ -6,3 +6,97 @@ PAYEES_LIST = [
     "GB6QNWJI4ERDKF3HPJOGHXN7IY5WE3BOIUGPSHJWPJSCLKQUQ57O5YFS",
     "GA3ABN3IJ2RKWWJVZDLPUUGRMDBBS23EIHPRBYAALLII2CAE5JAF7X36",
 ]
+
+STELLAR_ACCOUNT_RESPONSE = {
+    "_links": {
+        "self": {
+            "href": "https://horizon-testnet.stellar.org/accounts/GBZSWG2IZQHAEFEJNOQ6OUAICPVK7DO7R5GTAIOBGXFDCYU5PFYT3TA2"
+        },
+        "transactions": {
+            "href": "https://horizon-testnet.stellar.org/accounts/GBZSWG2IZQHAEFEJNOQ6OUAICPVK7DO7R5GTAIOBGXFDCYU5PFYT3TA2/transactions{?cursor,limit,order}",
+            "templated": True,
+        },
+        "operations": {
+            "href": "https://horizon-testnet.stellar.org/accounts/GBZSWG2IZQHAEFEJNOQ6OUAICPVK7DO7R5GTAIOBGXFDCYU5PFYT3TA2/operations{?cursor,limit,order}",
+            "templated": True,
+        },
+        "payments": {
+            "href": "https://horizon-testnet.stellar.org/accounts/GBZSWG2IZQHAEFEJNOQ6OUAICPVK7DO7R5GTAIOBGXFDCYU5PFYT3TA2/payments{?cursor,limit,order}",
+            "templated": True,
+        },
+        "effects": {
+            "href": "https://horizon-testnet.stellar.org/accounts/GBZSWG2IZQHAEFEJNOQ6OUAICPVK7DO7R5GTAIOBGXFDCYU5PFYT3TA2/effects{?cursor,limit,order}",
+            "templated": True,
+        },
+        "offers": {
+            "href": "https://horizon-testnet.stellar.org/accounts/GBZSWG2IZQHAEFEJNOQ6OUAICPVK7DO7R5GTAIOBGXFDCYU5PFYT3TA2/offers{?cursor,limit,order}",
+            "templated": True,
+        },
+        "trades": {
+            "href": "https://horizon-testnet.stellar.org/accounts/GBZSWG2IZQHAEFEJNOQ6OUAICPVK7DO7R5GTAIOBGXFDCYU5PFYT3TA2/trades{?cursor,limit,order}",
+            "templated": True,
+        },
+        "data": {
+            "href": "https://horizon-testnet.stellar.org/accounts/GBZSWG2IZQHAEFEJNOQ6OUAICPVK7DO7R5GTAIOBGXFDCYU5PFYT3TA2/data/{key}",
+            "templated": True,
+        },
+    },
+    "id": "GBZSWG2IZQHAEFEJNOQ6OUAICPVK7DO7R5GTAIOBGXFDCYU5PFYT3TA2",
+    "account_id": "GBZSWG2IZQHAEFEJNOQ6OUAICPVK7DO7R5GTAIOBGXFDCYU5PFYT3TA2",
+    "sequence": "2831444304986113",
+    "sequence_ledger": 659248,
+    "sequence_time": "1674473684",
+    "subentry_count": 1,
+    "last_modified_ledger": 659248,
+    "last_modified_time": "2023-01-23T11:34:44Z",
+    "thresholds": {"low_threshold": 0, "med_threshold": 0, "high_threshold": 0},
+    "flags": {
+        "auth_required": False,
+        "auth_revocable": False,
+        "auth_immutable": False,
+        "auth_clawback_enabled": False,
+    },
+    "balances": [
+        {
+            "balance": "10000000000.0000000",
+            "limit": "922337203685.4775807",
+            "buying_liabilities": "0.0000000",
+            "selling_liabilities": "0.0000000",
+            "last_modified_ledger": 659249,
+            "is_authorized": True,
+            "is_authorized_to_maintain_liabilities": True,
+            "asset_type": "credit_alphanum4",
+            "asset_code": "EUR",
+            "asset_issuer": "GC3XSOYSQDYBBGBBMV7OBZJOZQL47POSV6R4WPPXQVYMIEQBDFTAK46U",
+        },
+        {
+            "balance": "10.0000000",
+            "limit": "922337203685.4775807",
+            "buying_liabilities": "0.0000000",
+            "selling_liabilities": "0.0000000",
+            "last_modified_ledger": 659249,
+            "is_authorized": True,
+            "is_authorized_to_maintain_liabilities": True,
+            "asset_type": "credit_alphanum4",
+            "asset_code": "USD",
+            "asset_issuer": "GDGDATWYNYC5SBZORXK3JVU5NSIHSZWB72FWCBWYCQIMYS5V3BUK4PH7",
+        },
+        {
+            "balance": "9999.9990000",
+            "buying_liabilities": "0.0000000",
+            "selling_liabilities": "0.0000000",
+            "asset_type": "native",
+        },
+    ],
+    "signers": [
+        {
+            "weight": 1,
+            "key": "GBZSWG2IZQHAEFEJNOQ6OUAICPVK7DO7R5GTAIOBGXFDCYU5PFYT3TA2",
+            "type": "ed25519_public_key",
+        }
+    ],
+    "data": {},
+    "num_sponsoring": 0,
+    "num_sponsored": 0,
+    "paging_token": "GBZSWG2IZQHAEFEJNOQ6OUAICPVK7DO7R5GTAIOBGXFDCYU5PFYT3TA2",
+}
